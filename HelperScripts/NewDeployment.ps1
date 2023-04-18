206240f323964a62900ccf1e5341ed9c0039ac47
Zach Trocinski <ztrocinski@outlook.com>
2023-04-18 16:13:33 -0500

    Updated variables

--- HelperScripts/NewDeployment.ps1
+++ HelperScripts/NewDeployment.ps1
@@ -1,21 +1,21 @@
-$ResourceGroupName = 'rg-avd-weu-avd1-service-objects'
+$ResourceGroupName = 'function-gh-poc-rg'
 $bicepParams = @{
 
     #FunctionApp
-    FunctionAppName            = 'func-avdreplacementplan-weu-230131' # Name must be globally unique
-    HostPoolName               = 'vdpool-weu-avd1-001'
-    TargetSessionHostCount     = 2 # Replace this with your target number of session hosts in the pool
-    SessionHostNamePrefix      = "AVD-WE-D01"
-    SessionHostTemplateUri     = "https://raw.githubusercontent.com/WillyMoselhy/AVDReplacementPlans/main/SampleSessionHostTemplate/sessionhost.json"
-    FunctionAppZipUrl          = 'https://github.com/WillyMoselhy/AVDReplacementPlans/releases/download/v0.1.5/FunctionApp.zip'
-    ADOrganizationalUnitPath   = "OU=AVD,DC=contoso,DC=local"
-    SubnetId                   = "/subscriptions/2cc55a8e-7e60-4bba-b1e1-2241e5249d46/resourceGroups/rg-ActiveDirectory-01/providers/Microsoft.Network/virtualNetworks/rg-ActiveDirectory-01-vnet/subnets/default"
+    FunctionAppName           = 'func-avdreplacementplan-weu-230131' # Name must be globally unique
+    HostPoolName              = 'vdpool-weu-avd1-001'
+    TargetSessionHostCount    = 2 # Replace this with your target number of session hosts in the pool
+    SessionHostNamePrefix     = "AVD-WE-D01"
+    SessionHostTemplateUri    = "https://raw.githubusercontent.com/WillyMoselhy/AVDReplacementPlans/main/SampleSessionHostTemplate/sessionhost.json"
+    FunctionAppZipUrl         = 'https://github.com/WillyMoselhy/AVDReplacementPlans/releases/download/v0.1.5/FunctionApp.zip'
+    ADOrganizationalUnitPath  = "OU=AVD,DC=contoso,DC=local"
+    SubnetId                  = "/subscriptions/2cc55a8e-7e60-4bba-b1e1-2241e5249d46/resourceGroups/rg-ActiveDirectory-01/providers/Microsoft.Network/virtualNetworks/rg-ActiveDirectory-01-vnet/subnets/default"
 
     # Supporting Resources
-    StorageAccountName         = 'stavdreplacehost230131' # Make sure this is a unique name
-    LogAnalyticsWorkspaceName  = 'law-avdreplacementplan'
+    StorageAccountName        = 'functionghpocsa' # Make sure this is a unique name
+    LogAnalyticsWorkspaceName = 'law-avdreplacementplan'
     # Session Host Parameters
-    SessionHostParameters      = @{
+    SessionHostParameters     = @{
         VMSize                = 'Standard_B2ms'
         TimeZone              = 'GMT Standard Time'
         AdminUsername         = 'AVDAdmin'
